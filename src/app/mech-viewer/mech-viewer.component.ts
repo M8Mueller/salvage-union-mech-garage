abedd89f1a91cb9d290607e31981a752b0fb7547
Matt Mueller <mathieumueller@gmail.com>
2024-07-01 16:34:01 -0700

    final rough draft

--- src/app/mech-viewer/mech-viewer.component.ts
+++ src/app/mech-viewer/mech-viewer.component.ts
@@ -2,7 +2,9 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { debounceTime, pipe } from 'rxjs';
+import * as chassisData from '../chassis.json';
+import * as moduleData from '../modules.json';
+import * as systemData from '../systems.json';
 
 @Component({
   selector: 'app-mech-viewer',
@@ -16,6 +18,27 @@ import { debounceTime, pipe } from 'rxjs';
   styleUrl: './mech-viewer.component.css'
 })
 export class MechViewerComponent implements OnInit{
+  techLevels = [1, 2, 3, 4, 5, 6];
+
+  chassisList = chassisData.chassis;
+  moduleList = moduleData.modules;
+  systemList = systemData.systems;
+
+  chassis: any = null;
+  patternList: any[] = [];
+
+  systemSlotCount: number = 0;
+  moduleSlotCount: number = 0;
+
+  scrapCost: { [id: number]: number } = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0
+  }
+
   mechForm: FormGroup = this.fb.group({
     name: [''],
     pilot: [''],
@@ -25,185 +48,109 @@ export class MechViewerComponent implements OnInit{
     modules: this.fb.array([]),
   });
 
+  // Getters
+
+  getChassis(id: number) {
+    return this.chassisList.find((chas) => chas.id === id);
+  }
+
+  getPattern(id: number) {
+    return this.patternList.find((pat) => pat.id === id);
+  }
+
+  getSystem(id: number) {
+    return this.systemList.find((sys) => sys.id === id);
+  }
+
+  getModule(id: number) {
+    return this.moduleList.find((mod) => mod.id === id);
+  }
+
+  // Systems
+
   systemsFormArray() {
     return this.mechForm.get('systems') as FormArray;
   }
+
+  addSystem(id: number, setCustom: boolean = true) {
+    let system = this.getSystem(id);
+    this.addToFormArray(this.systemsFormArray(), system, setCustom);
+  }
+
+  removeSystem(index: number, setCustom: boolean = true) {
+    this.removeFromFormArray(this.systemsFormArray(), index, setCustom);
+  }
+
+  clearSystems(setCustom: boolean = true) {
+    this.clearFormArray(this.systemsFormArray(), setCustom);
+  }
+
+  // Modules
 
   modulesFormArray() {
     return this.mechForm.get('modules') as FormArray;
   }
 
-  chassis: any = null;
-  systemSlotCount: number = 0;
-  moduleSlotCount: number = 0;
+  addModule(id: number, setCustom: boolean = true) {
+    let module = this.getModule(id);
+    this.addToFormArray(this.modulesFormArray(), module, setCustom);
+  }
 
-  calculateSlotCounts() {
-    this.systemSlotCount = this.systemsFormArray().value.reduce(
-      (count: number, sys: any) => count + sys.slots,
-      0
+  removeModule(index: number, setCustom: boolean = true) {
+    this.removeFromFormArray(this.modulesFormArray(), index, setCustom);
+  }
+
+  clearModules(setCustom: boolean = true) {
+    this.clearFormArray(this.modulesFormArray(), setCustom);
+  }
+
+  // FormArray functions
+
+  addToFormArray(formArray: FormArray, item: any, setCustom: boolean = true) {
+    if (setCustom) {
+      this.mechForm.get('pattern')?.setValue(0);
+    }
+
+    formArray.push(
+      this.fb.group({
+        'id': item.id,
+        'name': item?.name,
+        'tech_level': item?.tech_level,
+        'slots': item?.slots,
+        'salvage_value': item?.salvage_value
+      })
     );
 
-    this.moduleSlotCount = this.modulesFormArray().value.reduce(
-      (count: number, mod: any) => count + mod.slots,
-      0
-    );
+    this.calculateValues();
   }
 
-  chassisList = [
-    {
-      'id': 1,
-      'name': 'Mule',
-      'structure_pts': 15,
-      'energy_pts': 6,
-      'heat_cap': 10,
-      'system_slots': 9,
-      'module_slots': 2,
-      'cargo_cap': 6,
-      'tech_level': 1,
-      'salvage_value': 9,
-      'ability': 'Heavily Armoured Chassis'
-    },
-    {
-      'id': 2,
-      'name': 'Mazona',
-      'structure_pts': 5,
-      'energy_pts': 10,
-      'heat_cap': 6,
-      'system_slots': 7,
-      'module_slots': 3,
-      'cargo_cap': 6,
-      'tech_level': 1,
-      'salvage_value': 4,
-      'ability': 'Hover Drone'
-    },
-  ];
-
-  getChassisStats(id: number) {
-    return this.chassisList.find((chas) => chas.id === id);
-  }
-
-  systemList = [
-    {
-      'id': 1,
-      'name': '.50 Cal Machine Gun',
-      'tech_level': 1,
-      'slots': 2,
-      'salvage_value': 2
-    },
-    {
-      'id': 2,
-      'name': 'Armour Plating',
-      'tech_level': 1,
-      'slots': 2,
-      'salvage_value': 1
-    },
-    {
-      'id': 3,
-      'name': 'Cargo Pod',
-      'tech_level': 1,
-      'slots': 1,
-      'salvage_value': 1
-    },
-    {
-      'id': 4,
-      'name': 'Chainsaw Arm',
-      'tech_level': 1,
-      'slots': 1,
-      'salvage_value': 1
+  removeFromFormArray(formArray: FormArray, index: number, setCustom: boolean = true) {
+    if (setCustom) {
+      this.mechForm.get('pattern')?.setValue(0);
     }
-  ]
 
-  getSystemStats(id: number) {
-    return this.systemList.find((sys) => sys.id === id);
+    formArray.removeAt(index);
+
+    this.calculateValues();
   }
 
-  addSystem(id: number) {
-    let system = this.getSystemStats(id);
-
-    this.systemsFormArray().push(this.fb.group({
-      'id': id,
-      'name': system?.name,
-      'tech_level': system?.tech_level,
-      'slots': system?.slots,
-      'salvage_value': system?.salvage_value
-    }));
-
-    this.calculateScrapCost();
-    this.calculateSlotCounts();
-  }
-
-  removeSystem(index: number) {
-    this.systemsFormArray().removeAt(index);
-
-    this.calculateScrapCost();
-    this.calculateSlotCounts();
-  }
-
-  moduleList = [
-    {
-      'id': 1,
-      'name': 'Comms Module',
-      'tech_level': 1,
-      'slots': 1,
-      'salvage_value': 1
-    },
-    {
-      'id': 2,
-      'name': 'Equipment Locker',
-      'tech_level': 1,
-      'slots': 1,
-      'salvage_value': 1
-    },
-    {
-      'id': 3,
-      'name': 'Firewall',
-      'tech_level': 1,
-      'slots': 1,
-      'salvage_value': 2
-    },
-    {
-      'id': 4,
-      'name': 'Personal Recreation Device',
-      'tech_level': 1,
-      'slots': 1,
-      'salvage_value': 1
+  clearFormArray(formArray: FormArray, setCustom: boolean = true) {
+    if (setCustom) {
+      this.mechForm.get('pattern')?.setValue(0);
     }
-  ]
 
-  getModuleStats(id: number) {
-    return this.moduleList.find((mod) => mod.id === id);
+    while (formArray.length !== 0) {
+      formArray.removeAt(0);
+    }
+
+    this.calculateValues();
   }
 
-  addModule(id: number) {
-    let module = this.getModuleStats(id);
-
-    this.modulesFormArray().push(this.fb.group({
-      'id': id,
-      'name': module?.name,
-      'tech_level': module?.tech_level,
-      'slots': module?.slots,
-      'salvage_value': module?.salvage_value
-    }));
-
+  // Calculations
+  
+  calculateValues() {
     this.calculateScrapCost();
     this.calculateSlotCounts();
-  }
-
-  removeModule(index: number) {
-    this.modulesFormArray().removeAt(index);
-
-    this.calculateScrapCost();
-    this.calculateSlotCounts();
-  }
-
-  techLevels = [1, 2, 3, 4, 5, 6]
-  scrapCost: { [id: number]: number } = {
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-    6: 0
   }
 
   calculateScrapCost() {
@@ -219,16 +166,43 @@ export class MechViewerComponent implements OnInit{
       (mod: any) => this.scrapCost[mod.tech_level] += mod.salvage_value);
   }
 
+  calculateSlotCounts() {
+    this.systemSlotCount = this.systemsFormArray().value.reduce(
+      (count: number, sys: any) => count + sys.slots,
+      0
+    );
+
+    this.moduleSlotCount = this.modulesFormArray().value.reduce(
+      (count: number, mod: any) => count + mod.slots,
+      0
+    );
+  }
+
   constructor(
     private fb: FormBuilder
   ) {
-    this.mechForm.get('chassis')?.valueChanges.pipe(
-      debounceTime(500),
-    ).subscribe((id) => {
-      this.chassis = this.getChassisStats(id);
+    this.mechForm.get('chassis')?.valueChanges.subscribe((id) => {
+      this.chassis = this.getChassis(id);
+      this.patternList = this.chassis.patterns;
+      if (this.mechForm.get('pattern')?.value > 1) {
+        this.mechForm.get('pattern')?.setValue(0);
+      }
 
-      this.calculateScrapCost();
-      this.calculateSlotCounts();
+      this.calculateValues();
+    });
+
+    this.mechForm.get('pattern')?.valueChanges.subscribe((id) => {
+      if (id === 0) return;
+
+      let pattern = this.getPattern(id);
+
+      this.clearSystems(false);
+      this.clearModules(false);
+
+      pattern.systems.forEach((sys: number) => this.addSystem(sys, false));
+      pattern.modules.forEach((sys: number) => this.addModule(sys, false));
+
+      this.calculateValues();
     });
   }
 
