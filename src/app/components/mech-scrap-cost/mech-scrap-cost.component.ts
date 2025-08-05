4ded236a08ff04cacd98b8800f16ac4511185fbf
Matt Mueller <mathieumueller@gmail.com>
2025-08-04 19:24:04 -0700

    data entry, visual tweaks

--- src/app/components/mech-scrap-cost/mech-scrap-cost.component.ts
+++ src/app/components/mech-scrap-cost/mech-scrap-cost.component.ts
@@ -4,6 +4,20 @@ import { Component, OnInit } from '@angular/core';
 import { CurrentMechService } from '../../services/current-mech.service';
 import { CardComponent } from "../elements/card/card.component";
 
+interface LeveledValue {
+  [level: number]: number,
+};
+
+function newLeveledValue(levels: number[]): LeveledValue {
+  let value: LeveledValue = {};
+
+  for (const level of levels) {
+    value[level] = 0;
+  }
+
+  return value;
+}
+
 
 @Component({
   selector: 'app-mech-scrap-cost',
@@ -16,6 +30,12 @@ import { CardComponent } from "../elements/card/card.component";
   styleUrl: './mech-scrap-cost.component.css'
 })
 export class MechScrapCostComponent implements OnInit {
+  levels = [1, 2, 3, 4, 5, 6];
+
+  leveledChassisScrapCost: LeveledValue = newLeveledValue(this.levels);
+  leveledSystemsScrapCost: LeveledValue = newLeveledValue(this.levels);
+  leveledModulesScrapCost: LeveledValue = newLeveledValue(this.levels);
+
   chassisScrapCost: number = 0;
   systemsScrapCost: number = 0;
   modulesScrapCost: number = 0;
@@ -25,9 +45,11 @@ export class MechScrapCostComponent implements OnInit {
   ngOnInit() {
     this.currentMech.chassis$.subscribe((chassis) => {
       this.chassisScrapCost = 0;
+      this.leveledChassisScrapCost = newLeveledValue(this.levels);
 
       if (chassis) {
         this.chassisScrapCost += chassis.tech_level * chassis.salvage_value;
+        this.leveledChassisScrapCost[chassis?.tech_level] = chassis.salvage_value;
       }
 
       this.calculateTotalScrapCost();
@@ -35,9 +57,11 @@ export class MechScrapCostComponent implements OnInit {
 
     this.currentMech.systems$.subscribe((systems) => {
       this.systemsScrapCost = 0;
+      this.leveledSystemsScrapCost = newLeveledValue(this.levels);
 
       systems.forEach((comp) => {
         this.systemsScrapCost += comp.tech_level * comp.salvage_value;
+        this.leveledSystemsScrapCost[comp.tech_level] += comp.salvage_value;
       });
 
       this.calculateTotalScrapCost();
@@ -45,9 +69,11 @@ export class MechScrapCostComponent implements OnInit {
 
     this.currentMech.modules$.subscribe((modules) => {
       this.modulesScrapCost = 0;
+      this.leveledModulesScrapCost = newLeveledValue(this.levels);
 
       modules.forEach((comp) => {
         this.modulesScrapCost += comp.tech_level * comp.salvage_value;
+        this.leveledModulesScrapCost[comp.tech_level] += comp.salvage_value;
       });
 
       this.calculateTotalScrapCost();
